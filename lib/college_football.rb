6062b8fa5fe05f4c0811651116970a24443aa9ef
Keith Filkins <kafilkins@me.com>
2021-01-12 00:41:10 +0000

    go bama

---
+++ lib/college_football.rb
@@ -0,0 +1,6 @@
+require "college_football/version"
+
+module CollegeFootball
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/college_football/version.rb
@@ -0,0 +1,3 @@
+module CollegeFootball
+  VERSION = "0.1.0"
+end
